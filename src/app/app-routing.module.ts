ed38494e7fa3b0ad151b03000e628bf968e2a8e2
saivitasoft <saikumarvita@gmail.com>
2023-11-19 14:24:04 +0530

    created buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'signin',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'signin',component:SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
