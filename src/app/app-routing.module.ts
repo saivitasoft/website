97d87ba3509cacfa47a3f0305f4caf8994dfaa34
saivitasoft <saikumarvita@gmail.com>
2023-12-02 14:52:31 +0530

    all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'signin',pathMatch:'full'},
+  {path:'',redirectTo:'main',pathMatch:'full'},
   {path:'login',component:LoginComponent},
-  {path:'signin',component:SigninComponent}
+  {path:'signin',component:SigninComponent},
+  {path:'home',component:HomeComponent},
+  {path:"main",component:MainComponent},
+  {path:'dash',component:DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdditemComponent } from './additem/additem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    MainComponent,
+    DashboardComponent,
+    AdditemComponent
   ],
   imports: [
     BrowserModule,
